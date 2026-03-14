e07799967075fe3307e150a5dc0eba9da168715e
Vince <61915948+vincent-sayah@users.noreply.github.com>
2026-03-14 20:09:42 +0100

    maj update

--- plugin.php
+++ plugin.php
@@ -3,8 +3,8 @@
 declare(strict_types=1);
 
 $id = 'acrm';
-$version = '0.2.0';
+$version = '1.0.0';
 $ilias_min_version = '10.0';
 $ilias_max_version = '10.999';
-$responsible = 'OpenAI';
-$responsible_mail = 'noreply@example.org';
+$responsible = 'Vincent Sayah';
+$responsible_mail = 'vince.syh@free.fr';
